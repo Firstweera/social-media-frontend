53bfd7f1c9f9bb2241d182ba89dca1d1d002b453
Weeraphat.w <weeraphat.w@siamraj.com>
2023-10-29 00:07:40 +0700

    fix/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Home, MainFeed, SignInCard, SignUpCard } from "./pages";
 import { Nav } from "./components";
 import { useEffect, useState } from "react";
 
 function App() {
+  const queryClient = new QueryClient();
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
@@ -21,16 +23,18 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Nav isAuthenticated={isAuthenticated} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<SignUpCard />} />
-          <Route path="/login" element={<SignInCard />} />
-          <Route path="/Main" element={<MainFeed />} />
-          <Route path="*" element={<>404 Not found</>} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Nav isAuthenticated={isAuthenticated} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<SignUpCard />} />
+            <Route path="/login" element={<SignInCard />} />
+            <Route path="/Main" element={<MainFeed />} />
+            <Route path="*" element={<>404 Not found</>} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
